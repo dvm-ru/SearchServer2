f3f7886041900222a674941b4878cae178285dbf
Григорий Аврамов <stabilat@gmail.com>
2022-02-26 12:59:38 +0300

    Спринт 8 Задача 2 из 3, в процессе...

--- SearchServer2/string_processing.cpp
+++ SearchServer2/string_processing.cpp
@@ -37,12 +37,12 @@
 //    return words;
 //}
 
-std::vector<std::string> SplitIntoWords(std::string_view text) {
-    std::vector<std::string> result;
+std::vector<std::string_view> SplitIntoWords(std::string_view text) {
+    std::vector<std::string_view> result;
     const int64_t pos_end = text.npos;
     while (true) {
         int64_t space = text.find(' ');
-        result.push_back(space == pos_end ? std::string{ text.substr(0) } : std::string{ text.substr(0, space) });
+        result.push_back(space == pos_end ? text.substr(0) : text.substr(0, space) );
         if (space == pos_end) {
             break;
         }
